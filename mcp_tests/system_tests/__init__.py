1838f4c85186e099c593972664dc0b22cffa59eb
Sergey Lebedev <slebedev@mirantis.com>
2016-07-19 13:18:37 +0300

    Switch to fixture using in tests
      My proposal is using fixtures for our tests. So, common actions are in
      mcp_tests/system_tests/base_test.py, fixtures described in
      mcp_tests/system_tests/conftest.py and test looks like
      mcp_tests/system_tests/test_kargo_deployed.py
    
      Updated command for kargo_deploy.sh test:
    
      ENV_NAME="<env_name>" IMAGE_PATH="<path_to_qcow2_image>" WORKSPACE=/tmp DEPLOY_SCRIPT="/path/to/kargo_deploy.sh" SUSPEND_ENV_ON_TEARDOWN=false py.test -vvv -s mcp_tests/system_tests/test_kargo_deployed.py
    
    Change-Id: I3f18a0463e6e0b2a2c20a83ef6c1a28f09beaeed

--- mcp_tests/system_tests/__init__.py
+++ mcp_tests/system_tests/__init__.py
@@ -11,39 +11,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-import logging
-
-import pytest
-
-from mcp_tests.helpers import mcp_tests_exceptions as exc
-from mcp_tests import logger
-from mcp_tests import settings
-
-logging.getLogger('EnvironmentManager').addHandler(logger.console)
-
-LOG = logger.logger
-
-
-class SystemBaseTest(object):
-    """SystemBaseTest contains setup/teardown for environment creation"""
-
-    @classmethod
-    def setup_class(cls):
-        """Create Environment or use an existing one"""
-        LOG.info('Trying to get existing environment')
-        try:
-            cls.env.get_env_by_name(name=settings.ENV_NAME)
-        except exc.EnvironmentDoesNotExist:
-            LOG.info("Environment doesn't exist, creating new one")
-            cls.env.create_environment()
-            LOG.info("Environment created")
-
-    @pytest.mark.skipif(not settings.SUSPEND_ENV_ON_TEARDOWN,
-                        reason="Suspend isn't needed"
-                        )
-    @classmethod
-    def teardown_class(cls):
-        """Suspend environment"""
-        LOG.info("Suspending environment")
-        cls.env.suspend()
---
+++ mcp_tests/system_tests/test_kargo_deployed.py
@@ -0,0 +1,67 @@
+import copy
+import os
+import subprocess
+
+import pytest
+import yaml
+
+from mcp_tests import logger
+from mcp_tests import settings
+from mcp_tests.system_tests import base_test
+
+LOG = logger.logger
+
+
+class TestKargoDeployedEnv(base_test.SystemBaseTest):
+    """Test class for testing environment deployed by Kargo"""
+    initial_snapshot = "empty"
+
+    kube_settings = {
+        "kube_network_plugin": "calico",
+        "kube_proxy_mode": "iptables",
+        "kube_version": settings.KUBE_VERSION,
+    }
+
+    def deploy_kargo(self, env, use_custom_yaml=False):
+        current_env = copy.deepcopy(os.environ)
+        environment_variables = {
+            "SLAVE_IPS": " ".join(env.k8s_ips),
+            "ADMIN_IP": env.k8s_ips[0],
+        }
+        if use_custom_yaml:
+            environment_variables.update(
+                {"CUSTOM_YAML": yaml.dump(
+                    self.kube_settings, default_flow_style=False)}
+            )
+        current_env.update(dict=environment_variables)
+        try:
+            process = subprocess.Popen([settings.DEPLOY_SCRIPT],
+                                       env=current_env,
+                                       shell=True,
+                                       bufsize=0,
+                                       )
+            assert process.wait() == 0
+        except (SystemExit, KeyboardInterrupt) as err:
+            process.terminate()
+            raise err
+
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot
+    @pytest.mark.fail_snapshot
+    @pytest.mark.usefixtures("kargo_deploy")
+    def test_deployed_kargo(self, env, k8sclient):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Create VMs using config from CONF_PATH system environment
+            variable, or get an existing environment.
+            2. Revert from initial snapshot to start deployment (if snapshot
+            exists)
+            3. Start deploying k8s via kargo_deploy.sh script, provided via
+            DEPLOY_SCRIPT environment variable
+            4. Check if nodes in k8s environment is equal to VMs nodes number.
+            5. Check if all of base containers exists on k8s nodes.
+        """
+        self.check_number_kube_nodes(env, k8sclient)
+        for node in env.k8s_nodes:
+            self.running_containers(node, env)
