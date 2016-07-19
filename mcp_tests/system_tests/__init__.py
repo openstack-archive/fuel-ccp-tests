6c17ce2761924d9336ffe014d477c73c779c74e0
Sergey Lebedev <slebedev@mirantis.com>
2016-07-19 13:18:37 +0300

    Switch to fixture using in tests
      My proposal is using fixtures for our tests. So, common actions are in
      mcp_tests/system_tests/__init__.py, fixtures described in
      mcp_tests/system_tests/conftest.py and test looks like
      mcp_tests/system_tests/test_kargo_deployed.py
    
      Updated command for kargo_deploy.sh test:
    
      ENV_NAME="<env_name>" IMAGE_PATH="<path_to_qcow2_image>" WORKSPACE=/tmp DEPLOY_SCRIPT="/path/to/kargo_deploy.sh" SUSPEND_ENV_ON_TEARDOWN=false py.test -vvv -s mcp_tests/system_tests/test_kargo_deployed.py
    
    Change-Id: I3f18a0463e6e0b2a2c20a83ef6c1a28f09beaeed

--- mcp_tests/system_tests/__init__.py
+++ mcp_tests/system_tests/__init__.py
@@ -12,11 +12,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import copy
 import logging
+import subprocess
+import os
 
 import pytest
+import yaml
 
-from mcp_tests.helpers import mcp_tests_exceptions as exc
 from mcp_tests import logger
 from mcp_tests import settings
 
@@ -24,20 +27,88 @@ logging.getLogger('EnvironmentManager').addHandler(logger.console)
 
 LOG = logger.logger
 
+LOG.addHandler(logger.console)
+
 
 class SystemBaseTest(object):
     """SystemBaseTest contains setup/teardown for environment creation"""
 
-    @classmethod
-    def setup_class(cls):
-        """Create Environment or use an existing one"""
-        LOG.info('Trying to get existing environment')
+    base_images = [
+        "calico/node",
+        "andyshinn/dnsmasq",
+        "quay.io/coreos/hyperkube"
+    ]
+
+    kube_settings = {
+        "kube_network_plugin": "calico",
+        "kube_proxy_mode": "iptables",
+        "kube_version": settings.KUBE_VERSION,
+    }
+
+    def running_containers(self, node):
+        """Check if there are all base containers on node
+
+        :param node: devops.models.Node
+        """
+        remote = self.env.node_ssh_client(
+            node,
+            **settings.SSH_NODE_CREDENTIALS
+        )
+        cmd = "docker ps --no-trunc --format '{{.Image}}'"
+        with remote.get_sudo(remote):
+            result = remote.execute(
+                command=cmd,
+                verbose=True
+            )
+            assert result['exit_code'] == 0
+        images = [x.split(":")[0] for x in result['stdout']]
+        assert set(self.base_images) < set(images)
+
+    def check_number_kube_nodes(self):
+        """Check number of slaves"""
+        LOG.info("Check number of nodes")
+        master = self.env.k8s_nodes[0]
+        remote = self.env.node_ssh_client(
+            master,
+            **settings.SSH_NODE_CREDENTIALS
+        )
+        cmd = "kubectl get nodes -o jsonpath={.items[*].metadata.name}"
+        result = remote.execute(command=cmd, verbose=True)
+        assert result["exit_code"] == 0, "Error: {0}".format(
+            "".join(result["stderr"])
+        )
+        k8s_nodes = result["stdout_str"].split()
+        devops_nodes = self.env.k8s_nodes
+        assert len(k8s_nodes) == len(devops_nodes)
+
+    def deploy_kargo(self, use_custom_yaml=False):
+        current_env = copy.deepcopy(os.environ)
+        environment_variables = {
+            "SLAVE_IPS": " ".join(self.env.k8s_ips),
+            "ADMIN_IP": self.env.k8s_ips[0],
+        }
+        if use_custom_yaml:
+            environment_variables.update(
+                {"CUSTOM_YAML": yaml.dump(
+                    self.kube_settings, default_flow_style=False)}
+            )
+        current_env.update(dict=environment_variables)
+        # assert self.env.has_snapshot(self.upgraded_snapshot)
+        # self.env.revert_snapshot(self.upgraded_snapshot)
         try:
-            cls.env.get_env_by_name(name=settings.ENV_NAME)
-        except exc.EnvironmentDoesNotExist:
-            LOG.info("Environment doesn't exist, creating new one")
-            cls.env.create_environment()
-            LOG.info("Environment created")
+            process = subprocess.Popen([settings.DEPLOY_SCRIPT],
+                                       env=current_env,
+                                       shell=True,
+                                       bufsize=0,
+                                       )
+            assert process.wait() == 0
+            # self.env.create_snapshot(self.deployed_snapshot)
+        except (SystemExit, KeyboardInterrupt) as err:
+            process.terminate()
+            raise err
+
+    def create_env_snapshot(self, name, description=None):
+        self.env.create_snapshot(name, description=description)
 
     @pytest.mark.skipif(not settings.SUSPEND_ENV_ON_TEARDOWN,
                         reason="Suspend isn't needed"
---
+++ mcp_tests/system_tests/conftest.py
@@ -0,0 +1,174 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from __future__ import division
+import time
+
+import pytest
+from devops import error
+
+from mcp_tests import logger
+from mcp_tests import settings
+from mcp_tests.managers import envmanager
+
+LOG = logger.logger
+
+
+def extract_name_from_mark(mark):
+    """Simple function to extract name from mark
+
+    :param mark: pytest.mark.MarkInfo
+    :rtype: string or None
+    """
+    if mark:
+        if len(mark.args) > 0:
+            return mark.args[0]
+        elif 'name' in mark.kwargs:
+            return mark.kwargs['name']
+    return None
+
+
+def make_snapshot_name(request, suffix, default):
+    """Creating snapshot name
+
+    :param request: pytest.python.FixtureRequest
+    :param suffix: string
+    :param default: string or None
+    :rtype: string or None
+    """
+    if request:
+        if suffix:
+            return "{0}_{1}".format(
+                request.instance.env.d_env_name,
+                suffix
+            )
+    return default
+
+
+@pytest.fixture(scope='function', autouse=True)
+def prepare(request):
+    """Fixture to prepare needed state and revert from snapshot if it's needed
+
+    :param request: pytest.python.FixtureRequest
+    """
+    revert_snapshot = request.keywords.get('revert_snapshot', None)
+    snapshot_name = make_snapshot_name(
+        request, extract_name_from_mark(revert_snapshot), None)
+    if snapshot_name:
+        if request.instance.env.has_snapshot(snapshot_name):
+            print("Reverting snapshot {0}".format(snapshot_name))
+            request.instance.env.revert_snapshot(snapshot_name)
+        elif revert_snapshot.kwargs.get('strict', False):
+            pytest
+    deploy_kargo = request.keywords.get('deploy_kargo', None)
+    if deploy_kargo:
+        request.instance.deploy_kargo(
+            use_custom_yaml=deploy_kargo.kwargs.get('use_custom_yaml', False))
+
+
+@pytest.fixture(scope="class", autouse=True)
+def environment(request):
+    """Class fixture to get or create environment
+
+    :param request: pytest.python.FixtureRequest
+    """
+    env = envmanager.EnvironmentManager(config_file=settings.CONF_PATH)
+    snapshot_suffix = request.cls.initial_snapshot
+    env_name = env.devops_config['env_name']
+    initial_snapshot_name = "{0}_{1}".format(
+        env_name,
+        snapshot_suffix
+    )
+    try:
+        env.get_env_by_name(env_name)
+    except error.DevopsObjNotFound:
+        LOG.info("Environment doesn't exist, creating a new one")
+        env.create_environment()
+        env.create_snapshot(initial_snapshot_name)
+        LOG.info("Environment created")
+    request.cls.env = env
+
+
+@pytest.fixture(scope='function', autouse=True)
+def snapshot(request):
+    """Fixture for creating snapshot at the end of test if it's needed
+
+    :param request: pytest.python.FixtureRequest
+    """
+    snapshot_needed = request.keywords.get('snapshot_needed', None)
+    fail_snapshot = request.keywords.get('fail_snapshot', None)
+
+    def test_fin():
+        if request.node.rep_call.passed:
+            if snapshot_needed:
+                snapshot_name = make_snapshot_name(
+                    request, extract_name_from_mark(snapshot_needed),
+                    request.node.function.__name__ + "_passed"
+                )
+                request.instance.create_env_snapshot(
+                    name=snapshot_name
+                )
+        elif request.node.rep_setup.failed:
+            if fail_snapshot:
+                suffix = "{0}_prep_failed".format(
+                    request.node.function.__name__
+                )
+                request.instance.create_env_snapshot(
+                    name=make_snapshot_name(
+                        request, suffix, None
+                    )
+                )
+        elif request.node.rep_call.failed:
+            if fail_snapshot:
+                suffix = "{0}_failed".format(
+                    request.node.function.__name__
+                )
+                snapshot_name = make_snapshot_name(
+                    request, suffix, None
+                )
+                request.instance.create_env_snapshot(
+                    name=snapshot_name
+                )
+    request.addfinalizer(test_fin)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+
+
+def pytest_runtest_setup(item):
+    item.cls._current_test = item.function
+    item._start_time = time.time()
+    head = "<" * 5 + "#" * 30 + "[ {} ]" + "#" * 30 + ">" * 5
+    head = head.format(item.function.__name__)
+    start_step = "\n{head}".format(head=head)
+    LOG.info(start_step)
+
+
+def pytest_runtest_teardown(item):
+    step_name = item.function.__name__
+    if hasattr(item, '_start_time'):
+        spent_time = time.time() - item._start_time
+    else:
+        spent_time = 0
+    minutes = spent_time // 60
+    seconds = int(round(spent_time)) % 60
+    finish_step = "FINISH {} TEST. TOOK {} min {} sec".format(
+        step_name, minutes, seconds
+    )
+    foot = "\n" + "<" * 5 + "#" * 30 + "[ {} ]" + "#" * 30 + ">" * 5
+    foot = foot.format(finish_step)
+    LOG.info(foot)
