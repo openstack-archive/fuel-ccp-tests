bf2b082c4c8c49232a0c0975810d8e4155c6aff0
Sergey Lebedev <slebedev@mirantis.com>
2016-07-19 13:18:37 +0300

    Switch to fixture using in tests
      My proposal is using fixtures for our tests. So, common actions are in
      mcp_tests/system_tests/base_test.py, fixtures described in
      mcp_tests/system_tests/*_fixtures.py, and test looks like
      mcp_tests/system_tests/test_ccp_install_k8s.py
    
      Updated command for test:
    
      ENV_NAME="<env_name>" IMAGE_PATH="<path_to_qcow2_image>" WORKSPACE=/tmp DEPLOY_SCRIPT="/path/to/kargo_deploy.sh" SUSPEND_ENV_ON_TEARDOWN=false py.test -vvv -s mcp_tests/system_tests/test_ccp_install_k8s.py
    
    Change-Id: I3f18a0463e6e0b2a2c20a83ef6c1a28f09beaeed

---
+++ mcp_tests/system_tests/test_ccp_install_k8s.py
@@ -0,0 +1,46 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import pytest
+
+from mcp_tests import logger
+from mcp_tests import settings
+import base_test
+
+LOG = logger.logger
+
+
+class TestFuelCCPInstaller(base_test.SystemBaseTest):
+    """Test class for testing k8s deployed by fuel-ccp-installer"""
+
+    kube_settings = {
+        "kube_network_plugin": "calico",
+        "kube_proxy_mode": "iptables",
+        "kube_version": settings.KUBE_VERSION,
+    }
+
+    # TODO(slebedev): Extend checks for k8s installation
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot
+    @pytest.mark.fail_snapshot
+    def test_k8s_installed(self, env, k8sclient, use_custom_yaml):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Install k8s.
+            2. Check number of nodes.
+            3. Basic check of running containers on nodes.
+        """
+        self.ccp_install_k8s(env, use_custom_yaml=use_custom_yaml)
+        self.check_number_kube_nodes(env, k8sclient)
+        self.check_running_containers(env)
