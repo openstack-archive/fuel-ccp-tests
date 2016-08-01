468934a86bbce96314e8f77fa8754d86b0fb9e6c
Sergey Lebedev <slebedev@mirantis.com>
2016-08-01 13:16:57 +0300

    Update custom_yaml generating
      So, we want to have an ability for providing custom_yaml at any step
      that we need, so I suggest to delete this object from base test, but
      keep this parameter in functions it needed.
    
    Change-Id: Id5479ff6acb5b8e6ec1cd165ada2410d326eaa14

--- mcp_tests/system_tests/base_test.py
+++ mcp_tests/system_tests/base_test.py
@@ -28,14 +28,6 @@ LOG.addHandler(logger.console)
 class SystemBaseTest(object):
     """SystemBaseTest contains setup/teardown for environment creation"""
 
-    kube_settings = {}
-
-    base_images = [
-        "quay.io/coreos/etcd",
-        "andyshinn/dnsmasq",
-        "quay.io/coreos/hyperkube"
-    ]
-
     def exec_on_node(self, env, node, cmd, expected_exit_code=0):
         """Function to exec command on node and get result
 
@@ -53,52 +45,54 @@ class SystemBaseTest(object):
                 command=cmd,
                 verbose=True
             )
-            assert result['exit_code'] == expected_exit_code
+            assert result['exit_code'] == expected_exit_code,\
+                "Failed command '{}' run on node '{}'".format(
+                    cmd, node.name)
         return result
 
-    def calico_ipip_exists(self, node, env):
+    def calico_ipip_exists(self, env):
         """Check if ipip is in calico pool config
 
         :param node: devops.models.Node
         :param env: mcp_tests.managers.envmanager.EnvironmentManager
         """
+        cmd = "calicoctl pool show | grep ipip"
         for node in env.k8s_nodes:
-            cmd = "calicoctl pool show | grep ipip"
             self.exec_on_node(env, node, cmd)
 
-    def running_containers(self, node, env, network_plugin):
+    def running_containers(self, node, env, required_images):
         """Check if there are all base containers on node
 
         :param node: devops.models.Node
         :param env: mcp_tests.managers.envmanager.EnvironmentManager
+        :param required_images: list
         """
         cmd = "docker ps --no-trunc --format '{{.Image}}'"
-        expected_list = copy.deepcopy(self.base_images)
-        if network_plugin == 'calico':
-            expected_list.append('calico/node')
         result = self.exec_on_node(env, node, cmd)
         images = [x.split(":")[0] for x in result['stdout']]
-        assert set(expected_list) < set(images)
+        assert set(required_images) < set(images),\
+            "Running containers check failed on node '{}'".format(node.name)
 
-    def check_running_containers(self, env, network_plugin=''):
+    def check_running_containers(self, env, required_images):
         """Check running containers on each node
 
         :param env: mcp_tests.managers.envmanager.EnvironmentManager
         """
         for node in env.k8s_nodes:
-            self.running_containers(node, env, network_plugin)
+            self.running_containers(node, env, required_images)
 
     def check_number_kube_nodes(self, env, k8sclient):
         """Check number of slaves"""
         LOG.info("Check number of nodes")
         k8s_nodes = k8sclient.nodes.list()
         devops_nodes = env.k8s_nodes
-        assert len(k8s_nodes) == len(devops_nodes)
+        assert len(k8s_nodes) == len(devops_nodes),\
+            "Check number k8s nodes failed!"
 
     def check_etcd_health(self, env):
         devops_nodes = env.k8s_nodes
         cmd = "etcdctl cluster-health | grep -c 'got healthy result'"
-        etcd_nodes = self.exec_on_node(env, env.k8s_nodes[0], cmd)['stdout']
+        etcd_nodes = self.exec_on_node(env, env.k8s_nodes[0], cmd)['stdout'][0]
         assert int(etcd_nodes) == len(devops_nodes),\
             "Number of etcd nodes is {0}," \
             " should be {1}".format(etcd_nodes, len(devops_nodes))
@@ -112,6 +106,10 @@ class SystemBaseTest(object):
         environment_variables = {
             "SLAVE_IPS": " ".join(env.k8s_ips),
             "ADMIN_IP": env.k8s_ips[0],
+            "ADMIN_USER": settings.SSH_LOGIN,
+            "ADMIN_PASSWORD": settings.SSH_PASSWORD,
+            "KARGO_REPO": settings.KARGO_REPO,
+            "KARGO_COMMIT": settings.KARGO_COMMIT,
         }
         if custom_yaml:
             environment_variables.update(
@@ -123,7 +121,7 @@ class SystemBaseTest(object):
         current_env.update(dict=environment_variables)
         self.deploy_k8s(environ=current_env)
 
-    def deploy_k8s(self, environ=os.environ):
+    def deploy_k8s(self, environ={}):
         """Base action to deploy k8s by external deployment script"""
         try:
             process = subprocess.Popen([settings.DEPLOY_SCRIPT],
@@ -131,7 +129,7 @@ class SystemBaseTest(object):
                                        shell=True,
                                        bufsize=0,
                                        )
-            assert process.wait() == 0
+            assert process.wait() == 0, "k8s deployment failed!"
         except (SystemExit, KeyboardInterrupt) as err:
             process.terminate()
             raise err
