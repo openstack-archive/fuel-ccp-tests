16750b702831f46c678c6cc270726253b477b730
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-08-18 16:07:08 +0300

    Fixtures based on oslo.config data objects
     - Added an initial implementation of oslo.config usage.
     - Fixtures:
         config - provides access to oslo.config objects
         hardware - (formelly: env) for manipulate hardware objects;
                    fixture initializes oslo.config SSH fields if
                    environment was deployed with fuel-devops;
         underlay - provides access to provisioned nodes with SSH
                    without dependency on fuel-devops nodes objects.
         k8scluster - provides access to K8sCluster.
                      If config.k8s.kube_host is empty - deploy cluster
         ccpcluster - provides access to CCPCluster (TODO)
                      if config.ccp.os_host is empty - deploy cluster
    
     - mcp_tests/conftest.py removed from root (use k8scluster instead)
     - mcp_tests/base_test.py removed from root (not used)
     - move k8s and ccp managers to mcp_tests/managers/ folder
    
    Change-Id: I8f15a884c7c5bcb402074fe80f399cfbb51cc1ef

---
+++ mcp_tests/system_tests/ccp_fixtures.py
@@ -0,0 +1,62 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import pytest
+
+from mcp_tests.helpers import ext
+from mcp_tests.managers import ccpmanager
+
+
+@pytest.fixture(scope='session')
+def ccp_actions(config, underlay):
+    """Fixture that provides various actions for CCP
+
+    :param config: fixture provides oslo.config
+    :param underlay: fixture provides underlay manager
+    :rtype: CCPManager
+
+    For use in tests or fixtures to deploy a custom CCP
+    """
+    return ccpmanager.CCPManager(config, underlay)
+
+
+@pytest.fixture(scope='session')
+def ccpcluster(config, hardware, underlay, k8scluster, ccp_actions):
+    """Fixture to get or install fuel-ccp on k8s environment
+
+    :param config: fixture provides oslo.config
+    :param hardware: fixture provides enviromnet manager
+    :param underlay: fixture provides underlay manager
+    :param k8scluster: fixture provides an installed k8s cluster
+    :param ccp_actions: fixture provides CCPManager instance
+    :rtype: CCPManager
+
+    If config.ccp.os_host is not set, this fixture assumes that
+    the ccp cluster was not deployed, and do the following:
+    - deploy ccp cluster
+    - make snapshot with name 'ccp_deployed'
+    - (TODO)return CCPCluster instance, None at the moment
+
+    If config.ccp.os_host was set, this fixture assumes that the ccp
+    cluster was already deployed, and do the following:
+    - (TODO)return CCPCluster instance, None at the moment
+
+    If you want to revert 'ccp_deployed' snapshot, please use mark:
+    @pytest.mark.revert_snapshot("ccp_deployed")
+    """
+    if config.ccp.os_host is None:
+        ccp_actions.install_ccp()
+        config.ccp.os_host = "TODO: get OpenStack endpoints"
+        hardware.create_snapshot(ext.SNAPSHOT.ccp_deployed)
+
+    return ccp_actions
