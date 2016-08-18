267bdd89cd0e74c955ebd5969fd883ce9c89d09f
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-08-18 19:54:46 +0300

    Move tests into a ./tests folder
    - K8sClient moved from ./models to ./managers
    - all YAMLs placed to ./tests/fixtures/templates
    - all tests placed to ./tests/ into separated folders depending
      on tests types
    - ssh_manager removed from helpers (unused)
    - main folder 'mcp_tests' renamed to 'fuel_ccp_tests'
    
    Also, the tests now can be installed as a PyPI package.
    A very basic runner 'fuel_ccp_tests' added, with no logic yet.
    
    Change-Id: I25671dd7d6f3ee294a4e4146f9f2c2ba4d9b2d06

---
+++ fuel_ccp_tests/managers/k8s/cluster.py
@@ -0,0 +1,104 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+
+
+import base64
+import ssl
+
+from k8sclient.client import api_client
+from k8sclient.client.apis import apiv_api
+from k8sclient.client.apis import apisextensionsvbeta_api
+from k8sclient.client.apis import apisbatchv_api
+
+from fuel_ccp_tests.managers.k8s.componentstatuses import \
+    K8sComponentStatusManager
+from fuel_ccp_tests.managers.k8s.daemonsets import K8sDaemonSetManager
+from fuel_ccp_tests.managers.k8s.deployments import K8sDeploymentManager
+from fuel_ccp_tests.managers.k8s.endpoints import K8sEndpointManager
+from fuel_ccp_tests.managers.k8s.events import K8sEventManager
+from fuel_ccp_tests.managers.k8s.horizontalpodautoscalers import \
+    K8sHorizontalPodAutoscalerManager
+from fuel_ccp_tests.managers.k8s.ingresses import K8sIngressManager
+from fuel_ccp_tests.managers.k8s.jobs import K8sJobManager
+from fuel_ccp_tests.managers.k8s.limitranges import K8sLimitRangeManager
+from fuel_ccp_tests.managers.k8s.namespaces import K8sNamespaceManager
+from fuel_ccp_tests.managers.k8s.nodes import K8sNodeManager
+from fuel_ccp_tests.managers.k8s.persistentvolumeclaims import \
+    K8sPersistentVolumeClaimManager
+from fuel_ccp_tests.managers.k8s.persistentvolumes import \
+    K8sPersistentVolumeManager
+from fuel_ccp_tests.managers.k8s.pods import K8sPodManager
+from fuel_ccp_tests.managers.k8s.replicationcontrollers import \
+    K8sReplicationControllerManager
+from fuel_ccp_tests.managers.k8s.resourcequotas import K8sResourceQuotaManager
+from fuel_ccp_tests.managers.k8s.secrets import K8sSecretManager
+from fuel_ccp_tests.managers.k8s.serviceaccounts import K8sServiceAccountManager
+from fuel_ccp_tests.managers.k8s.services import K8sServiceManager
+
+
+class K8sCluster(object):
+    """docstring for K8sCluster"""
+
+    def __init__(self, schema="https", user=None, password=None,
+                 host='localhost', port='443', default_namespace='default'):
+        if user and password:
+            auth = base64.encodestring('%s:%s' % (user, password))[:-1]
+            auth = "Basic {}".format(auth)
+            self._client = api_client.ApiClient(
+                '{schema}://{host}:{port}/'.format(
+                    schema=schema, host=host, port=port))
+            self._client.set_default_header('Authorization', auth)
+            restcli_impl = self._client.RESTClient.IMPL
+            restcli_impl.ssl_pool_manager.connection_pool_kw['cert_reqs'] = \
+                ssl.CERT_NONE
+
+        else:
+            self._client = api_client.ApiClient(
+                '{schema}://{host}:{port}/'.format(
+                    schema=schema, host=host, port=port))
+        self._api = apiv_api.ApivApi(self._client)
+        self._bapi = apisbatchv_api.ApisbatchvApi(self._client)
+        self._eapi = apisextensionsvbeta_api.ApisextensionsvbetaApi(
+            self._client)
+        self._default_namespace = default_namespace
+
+        self.nodes = K8sNodeManager(self._api, self._default_namespace)
+        self.pods = K8sPodManager(self._api, self._default_namespace)
+        self.endpoints = K8sEndpointManager(self._api, self._default_namespace)
+        self.namespaces = K8sNamespaceManager(self._api,
+                                              self._default_namespace)
+        self.services = K8sServiceManager(self._api, self._default_namespace)
+        self.serviceaccounts = K8sServiceAccountManager(
+            self._api, self._default_namespace)
+        self.secrets = K8sSecretManager(self._api, self._default_namespace)
+        self.events = K8sEventManager(self._api, self._default_namespace)
+        self.limitranges = K8sLimitRangeManager(self._api,
+                                                self._default_namespace)
+        self.jobs = K8sJobManager(self._bapi, self._default_namespace)
+        self.daemonsets = K8sDaemonSetManager(self._eapi,
+                                              self._default_namespace)
+        self.ingresses = K8sIngressManager(self._eapi, self._default_namespace)
+        self.deployments = K8sDeploymentManager(self._eapi,
+                                                self._default_namespace)
+        self.horizontalpodautoscalers = K8sHorizontalPodAutoscalerManager(
+            self._eapi, self._default_namespace)
+        self.componentstatuses = K8sComponentStatusManager(
+            self._api, self._default_namespace)
+        self.resourcequotas = K8sResourceQuotaManager(
+            self._api, self._default_namespace)
+        self.replicationcontrollers = K8sReplicationControllerManager(
+            self._api, self._default_namespace)
+        self.pvolumeclaims = K8sPersistentVolumeClaimManager(
+            self._api, self._default_namespace)
+        self.pvolumes = K8sPersistentVolumeManager(
+            self._api, self._default_namespace)
---
+++ fuel_ccp_tests/tests/system/test_deploy.py
@@ -0,0 +1,174 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import os
+import pytest
+import yaml
+
+import base_test
+from fuel_ccp_tests import logger
+from fuel_ccp_tests import settings
+from fuel_ccp_tests.helpers import post_install_k8s_checks
+from fuel_ccp_tests.helpers import post_os_deploy_checks
+from fuel_ccp_tests.helpers import ext
+from fuel_ccp_tests.managers import k8smanager
+from fuel_ccp_tests.managers import ccpmanager
+
+LOG = logger.logger
+
+
+class TestDeployOpenstack(base_test.SystemBaseTest):
+    """Create VMs for mcpinstaller"""
+
+    snapshot_microservices_deployed = 'snapshot_microservices_deployed'
+    kube_settings = {
+        "kube_network_plugin": "calico",
+        "kube_proxy_mode": "iptables",
+        "hyperkube_image_repo": "quay.io/coreos/hyperkube",
+        "hyperkube_image_tag": "{0}_coreos.0".format(settings.KUBE_VERSION),
+        "kube_version": settings.KUBE_VERSION,
+        "ipip": settings.IPIP_USAGE,
+        "images_namespace": settings.IMAGES_NAMESPACE,
+        "docker_options": settings.REGISTRY,
+        "docker_options": "--insecure-registry={0}".format(settings.REGISTRY),
+        "upstream_dns_servers": settings.UPSTREAM_DNS,
+    }
+
+    def get_params(self, params_list, exclude_list):
+        params = [param for param in params_list if param not in exclude_list]
+        return params
+
+    def create_registry(self, remote):
+        registry_pod = os.getcwd() + '/fuel_ccp_tests/tests/fixtures/templates/' \
+                                     'registry_templates/registry-pod.yaml'
+        service_registry = os.getcwd() + '/fuel_ccp_tests/tests/fixtures/' \
+                                         'templates/registry_templates/' \
+                                         'service-registry.yaml'
+        for item in registry_pod, service_registry:
+            remote.upload(item, './')
+        command = [
+            'kubectl create -f ~/{0}'.format(registry_pod.split('/')[-1]),
+            'kubectl create -f ~/{0}'.format(
+                service_registry.split('/')[-1]),
+        ]
+        for cmd in command:
+            LOG.info(
+                "Running command '{cmd}' on node {node}".format(
+                    cmd=cmd,
+                    node=remote.hostname
+                )
+            )
+            result = remote.execute(cmd)
+            assert result['exit_code'] == 0
+
+    def pre_build_deploy_step(self, remote):
+        topology_path = os.getcwd() + '/fuel_ccp_tests/tests/fixtures/templates/' \
+                                      'k8s_templates/k8s_topology.yaml'
+        remote.upload(topology_path, './')
+        command = '>/var/log/microservices.log'
+        with remote.get_sudo(remote):
+            LOG.info(
+                "Running command '{cmd}' on node {node}".format(
+                    cmd=command,
+                    node=remote.hostname
+                )
+            )
+            result = remote.check_call(command, verbose=True)
+            assert result['exit_code'] == 0
+
+    @pytest.mark.snapshot_needed(name=snapshot_microservices_deployed)
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.fail_snapshot
+    def test_fuel_ccp_deploy_microservices(self, config, underlay):
+        """Deploy base environment
+
+        Scenario:
+        1. Revert snapshot
+        2. Install microservices
+        3. Deploy environment
+        4. Check deployment
+
+        Duration 30 min
+        """
+        config.k8s.kube_host = k8smanager.K8SManager.install_k8s(
+            underlay, custom_yaml=self.kube_settings)
+        ccpmanager.CCPManager.install_ccp(underlay, config)
+        k8sclient = k8smanager.K8SManager.get_k8sclient(config)
+
+        remote = underlay.remote(host=config.k8s.kube_host)
+        self.pre_build_deploy_step(remote)
+
+        registry = None
+        if settings.BUILD_IMAGES:
+            registry = '127.0.0.1:31500'
+            self.create_registry(remote)
+            exclude_list = ['deploy']
+            yaml_path = os.getcwd() + '/fuel_ccp_tests/tests/fixtures/templates/' \
+                                      'k8s_templates/build-deploy_cluster.yaml'
+            with open(yaml_path, 'r') as yaml_path:
+                params_list = yaml.load(yaml_path)['ccp-microservices-options']
+                params = self.get_params(params_list, exclude_list)
+                data = ' '.join(params)
+                data = data.format(registry_address=registry,
+                                   images_namespace=settings.IMAGES_NAMESPACE,
+                                   images_tag=settings.IMAGES_TAG)
+            command = [
+                'ccp {0}'.format(data)
+            ]
+            with remote.get_sudo(remote):
+                for cmd in command:
+                    LOG.info(
+                        "Running command '{cmd}' on node {node}".format(
+                            cmd=cmd,
+                            node=remote.hostname
+                        )
+                    )
+                    result = remote.execute(cmd)
+                    assert result['exit_code'] == 0
+            post_install_k8s_checks.check_calico_network(remote, k8sclient)
+        else:
+            registry = settings.REGISTRY
+            if not registry:
+                raise ValueError("The REGISTRY variable should be set with "
+                                 "external registry address, "
+                                 "current value {0}".format(settings.REGISTRY))
+
+        exclude_list = ['build', '--builder-push', '--builder-workers 1']
+        yaml_path = os.getcwd() + '/fuel_ccp_tests/tests/fixtures/templates/' \
+                                  'k8s_templates/build-deploy_cluster.yaml'
+
+        with open(yaml_path, 'r') as yaml_path:
+            params_list = yaml.load(yaml_path)['ccp-microservices-options']
+            params = self.get_params(params_list, exclude_list)
+            data = ' '.join(params)
+            data = data.format(
+                registry_address=registry,
+                images_namespace=settings.IMAGES_NAMESPACE,
+                images_tag=settings.IMAGES_TAG)
+        command = [
+            'ccp {0}'.format(data),
+        ]
+        with remote.get_sudo(remote):
+            for cmd in command:
+                LOG.info(
+                    "Running command '{cmd}' on node {node}".format(
+                        cmd=cmd,
+                        node=remote.hostname
+                    )
+                )
+                result = remote.execute(cmd)
+                assert result['exit_code'] == 0
+        post_os_deploy_checks.check_jobs_status(k8sclient, timeout=1500,
+                                                namespace='ccp')
+        post_os_deploy_checks.check_pods_status(k8sclient, timeout=1500,
+                                                namespace='ccp')
