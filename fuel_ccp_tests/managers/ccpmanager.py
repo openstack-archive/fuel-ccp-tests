3c1c1cf1e24f7eaded80e1ae70f45904339d3c91
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-08-19 14:25:33 +0300

    Move tests into a ./tests folder
    - K8sClient moved from ./models to ./managers
    - all YAMLs placed to ./tests/fixtures/templates
    - all tests placed to ./tests/ into separated folders depending
      on tests types
    - ssh_manager removed from helpers (unused)
    - main folder 'mcp_tests' renamed to 'fuel_ccp_tests'
    
    Also, the tests now can be installed as a PyPI package.
    A very basic runner 'fuel_ccp_tests' added, with no logic yet.
    
    Change-Id: I25671dd7d6f3ee294a4e4146f9f2c2ba4d9b2d06

---
+++ fuel_ccp_tests/managers/ccpmanager.py
@@ -0,0 +1,37 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from fuel_ccp_tests import settings
+from fuel_ccp_tests import logger
+
+
+LOG = logger.logger
+
+
+class CCPManager(object):
+    """docstring for CCPManager"""
+
+    def __init__(self):
+        super(CCPManager, self).__init__()
+
+    @classmethod
+    def install_ccp(cls, underlay, config):
+        """Base action to deploy k8s by external deployment script"""
+        LOG.info("Trying to install fuel-ccp on admin node")
+        remote = underlay.remote(host=config.k8s.kube_host)
+
+        ccp_repo_url = settings.CCP_REPO
+        cmd = ('pip install --upgrade git+{}'.format(ccp_repo_url))
+        with remote.get_sudo(remote):
+            remote.check_call(cmd, verbose=True)
+        remote.close()
