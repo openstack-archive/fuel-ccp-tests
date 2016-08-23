894bc585cb373c90f26fab1a22d958333441c560
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-08-23 19:14:18 +0300

    Move tests into a ./tests folder
    - K8sClient moved from ./models to ./managers
    - all YAMLs placed to ./tests/fixtures/templates
    - all tests placed to ./tests/ into separated folders depending
      on tests types
    - ssh_manager removed from helpers (unused)
    - main folder 'mcp_tests' renamed to 'fuel_ccp_tests'
    
    Also, the tests now can be installed as a PyPI package.
    A very basic runner 'fuel_ccp_tests' added, with no logic yet.
    
    Change-Id: I25671dd7d6f3ee294a4e4146f9f2c2ba4d9b2d06

---
+++ fuel_ccp_tests/fixtures/common_fixtures.py
@@ -0,0 +1,65 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import division
+import time
+
+import pytest
+
+from fuel_ccp_tests import logger
+from fuel_ccp_tests.helpers import utils
+
+LOG = logger.logger
+
+
+@pytest.yield_fixture(scope='session')
+def ssh_keys_dir(request):
+    ssh_keys_dir = utils.generate_keys()
+    LOG.info("SSH keys were generated in {}".format(ssh_keys_dir))
+    yield ssh_keys_dir
+    utils.clean_dir(ssh_keys_dir)
+    LOG.info("Tmp dir {} with generated ssh keys was cleaned".format(
+        ssh_keys_dir))
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+
+
+def pytest_runtest_setup(item):
+    item.cls._current_test = item.function
+    item._start_time = time.time()
+    head = "<" * 5 + "#" * 30 + "[ {} ]" + "#" * 30 + ">" * 5
+    head = head.format(item.function.__name__)
+    start_step = "\n{head}".format(head=head)
+    LOG.info(start_step)
+
+
+def pytest_runtest_teardown(item):
+    step_name = item.function.__name__
+    if hasattr(item, '_start_time'):
+        spent_time = time.time() - item._start_time
+    else:
+        spent_time = 0
+    minutes = spent_time // 60
+    seconds = int(round(spent_time)) % 60
+    finish_step = "FINISH {} TEST. TOOK {} min {} sec".format(
+        step_name, minutes, seconds
+    )
+    foot = "\n" + "<" * 5 + "#" * 30 + "[ {} ]" + "#" * 30 + ">" * 5
+    foot = foot.format(finish_step)
+    LOG.info(foot)
---
+++ fuel_ccp_tests/tests/system/test_ccp_install_k8s.py
@@ -0,0 +1,211 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import pytest
+
+import base_test
+from fuel_ccp_tests import logger
+from fuel_ccp_tests import settings
+from fuel_ccp_tests.helpers import ext
+
+LOG = logger.logger
+
+
+class FuelCCPInstallerConfigMixin:
+
+    kube_settings = settings.DEFAULT_CUSTOM_YAML
+
+    base_images = [
+        "andyshinn/dnsmasq",
+        "calico/node",
+    ]
+
+    custom_yaml_images = base_images + [kube_settings['hyperkube_image_repo']]
+
+
+class TestFuelCCPInstaller(base_test.SystemBaseTest,
+                           FuelCCPInstallerConfigMixin):
+    """Test class for testing k8s deployed by fuel-ccp-installer"""
+
+    @staticmethod
+    def get_nginx_spec(k8s_node=None):
+        """Create spec for k8s pod creation
+
+        :param k8s_node: fuel_ccp_tests.managers.k8s.nodes.K8sNode
+        """
+        pod = {
+            "apiVersion": "v1",
+            "kind": "Pod",
+            "metadata": {"name": "nginx"},
+            "spec": {
+                "containers": [
+                    {"name": "nginx",
+                     "image": "nginx",
+                     "ports": [{"containerPort": 80}]}
+                ],
+            }
+        }
+        if k8s_node:
+            pod['spec']['nodeName'] = k8s_node.name
+        return pod
+
+    @staticmethod
+    def check_nginx_pod_is_reached(underlay, ip, node_name=None):
+        """Simple check that nginx could be reached
+
+        :param underlay: fuel_ccp_tests.managers.UnderlaySSHManager
+        :param ip: string
+        :param node_name: string
+        """
+        env_node = node_name or underlay.node_names()[0]
+        cmd = "curl http://{}".format(ip)
+        underlay.sudo_check_call(command=cmd, node_name=env_node, verbose=True)
+
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.fail_snapshot
+    def test_k8s_installed_default(self, underlay, k8s_actions):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Install k8s.
+            2. Check number of nodes.
+            3. Basic check of running containers on nodes.
+            4. Check requirement base settings.
+            5. Create nginx pod.
+            6. Check created pod is reached
+            7. Delete pod.
+        """
+        k8s_actions.install_k8s()
+        k8sclient = k8s_actions.get_k8sclient()
+
+        self.check_number_kube_nodes(underlay, k8sclient)
+        self.check_list_required_images(
+            underlay, required_images=self.base_images)
+        self.calico_ipip_exists(underlay)
+        self.check_etcd_health(underlay)
+        nginx = self.get_nginx_spec()
+        pod = self.check_pod_create(body=nginx, k8sclient=k8sclient)
+        self.check_nginx_pod_is_reached(underlay, pod.status.pod_ip)
+        self.check_pod_delete(pod, k8sclient)
+
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.fail_snapshot
+    def test_k8s_installed_with_etcd_on_host(self, underlay, k8s_actions):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Install k8s with forced etcd on host.
+            2. Check number of nodes.
+            3. Basic check of running containers on nodes.
+            4. Check requirement base settings.
+            5. Create nginx pod.
+            6. Check created pod is reached
+            7. Delete pod.
+        """
+        kube_settings = dict()
+        kube_settings.update(self.kube_settings)
+        kube_settings.update({
+            'etcd_deployment_type': 'host',
+            'kube_network_plugin': 'calico'
+        })
+        required_images = filter(
+            lambda x: x != kube_settings.get(
+                'etcd_image_repo', settings.ETCD_IMAGE_REPO),
+            self.custom_yaml_images)
+
+        k8s_actions.install_k8s(custom_yaml=kube_settings)
+        k8sclient = k8s_actions.get_k8sclient()
+
+        self.check_number_kube_nodes(underlay, k8sclient)
+        self.check_list_required_images(underlay,
+                                        required_images=required_images)
+        self.calico_ipip_exists(underlay)
+        self.check_etcd_health(underlay)
+        nginx = self.get_nginx_spec()
+        pod = self.check_pod_create(body=nginx, k8sclient=k8sclient)
+        self.check_nginx_pod_is_reached(underlay, pod.status.pod_ip)
+        self.check_pod_delete(pod, k8sclient)
+
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.fail_snapshot
+    def test_k8s_installed_with_etcd_in_container(self, underlay, k8s_actions):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Install k8s with forced etcd in container.
+            2. Check number of nodes.
+            3. Basic check of running containers on nodes.
+            4. Check requirement base settings.
+            5. Create nginx pod.
+            6. Check created pod is reached
+            7. Delete pod.
+        """
+        kube_settings = dict()
+        kube_settings.update(self.kube_settings)
+        kube_settings.update({
+            'etcd_deployment_type': 'docker',
+            'kube_network_plugin': 'calico',
+            'etcd_image_repo': settings.ETCD_IMAGE_REPO,
+            'etcd_image_tag': settings.ETCD_IMAGE_TAG,
+        })
+        required_images = list(self.base_images)
+        required_images.append(kube_settings['etcd_image_repo'])
+
+        k8s_actions.install_k8s(custom_yaml=kube_settings)
+        k8sclient = k8s_actions.get_k8sclient()
+
+        self.check_number_kube_nodes(underlay, k8sclient)
+        self.check_list_required_images(underlay,
+                                        required_images=required_images)
+        self.calico_ipip_exists(underlay)
+        self.check_etcd_health(underlay)
+        nginx = self.get_nginx_spec()
+        pod = self.check_pod_create(body=nginx, k8sclient=k8sclient)
+        self.check_nginx_pod_is_reached(underlay, pod.status.pod_ip)
+        self.check_pod_delete(pod, k8sclient)
+
+    @pytest.mark.snapshot_needed
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.fail_snapshot
+    def test_k8s_installed_with_ready_ssh_keys(self, ssh_keys_dir,
+                                               underlay, k8s_actions):
+        """Test for deploying an k8s environment and check it
+
+        Scenario:
+            1. Install k8s (with prepared ssh keys).
+            2. Check number of nodes.
+            3. Basic check of running containers on nodes.
+            4. Check requirement base settings.
+            5. Create nginx pod.
+            6. Check created pod is reached
+            7. Delete pod.
+        """
+        add_var = {
+            "WORKSPACE": ssh_keys_dir
+        }
+
+        k8s_actions.install_k8s(env_var=add_var)
+        k8sclient = k8s_actions.get_k8sclient()
+
+        self.check_number_kube_nodes(underlay, k8sclient)
+        self.check_list_required_images(
+            underlay, required_images=self.base_images)
+        self.calico_ipip_exists(underlay)
+        self.check_etcd_health(underlay)
+        nginx = self.get_nginx_spec()
+        pod = self.check_pod_create(body=nginx, k8sclient=k8sclient)
+        self.check_nginx_pod_is_reached(underlay, pod.status.pod_ip)
+        self.check_pod_delete(pod, k8sclient)
