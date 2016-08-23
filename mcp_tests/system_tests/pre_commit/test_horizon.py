6bf14a8de25358b3054b7c06974e9f4a03e830de
Dennis Dmitriev <ddmitriev@mirantis.com>
2016-08-23 16:08:43 +0300

    Fixtures based on oslo.config data objects
     - Added an initial implementation of oslo.config usage.
     - Fixtures:
         config - provides access to oslo.config objects
         hardware - (formelly: env) for manipulate hardware objects;
                    fixture initializes oslo.config SSH fields if
                    environment was deployed with fuel-devops;
         underlay - provides access to provisioned nodes with SSH
                    without dependency on fuel-devops nodes objects.
         k8scluster - provides access to K8sCluster.
                      If config.k8s.kube_host is empty - deploy cluster
         ccpcluster - provides access to CCPCluster (TODO)
                      if config.ccp.os_host is empty - deploy cluster
    
     - mcp_tests/conftest.py removed from root (use k8scluster instead)
     - mcp_tests/base_test.py removed from root (not used)
     - move k8s and ccp managers to mcp_tests/managers/ folder
    
    Change-Id: I8f15a884c7c5bcb402074fe80f399cfbb51cc1ef

--- mcp_tests/system_tests/pre_commit/test_horizon.py
+++ mcp_tests/system_tests/pre_commit/test_horizon.py
@@ -17,15 +17,13 @@ import pytest
 from mcp_tests import logger
 from mcp_tests import settings
 from mcp_tests.helpers import post_os_deploy_checks
-from mcp_tests.managers import k8s
-from mcp_tests.managers import ccp
-import base_test
+from mcp_tests.helpers import ext
 
 LOG = logger.logger
 LOG.addHandler(logger.console)
 
 
-class TestServiceHorizon(base_test.ServiceBase):
+class TestServiceHorizon(object):
 
     dockerfile = '''
     FROM {registry}/{tag}/horizon
@@ -35,9 +33,10 @@ class TestServiceHorizon(base_test.ServiceBase):
      /horizon-master/openstack_dashboard/test/integration_tests
     '''.format(registry=settings.REGISTRY, tag='ccp')
 
-    @pytest.mark.revert_snapshot
-    @pytest.mark.horizon_test
-    def test_horizon_component(self, env, k8sclient):
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.initial)
+    @pytest.mark.horizon_component
+    def test_horizon_component(self, config, underlay,
+                               k8s_actions, ccp_actions):
         """Horizon pre-commit test
         Scenario:
         1. Install k8s
@@ -53,23 +52,22 @@ class TestServiceHorizon(base_test.ServiceBase):
         11. Run horizon tests from docker
         Duration 60 min
         """
-        k8s.K8SManager.install_k8s(env)
-        ccp.CCPManager.install_ccp(env)
-        k8s_node = env.k8s_nodes[0]
-        remote = env.node_ssh_client(
-            k8s_node,
-            **settings.SSH_NODE_CREDENTIALS)
-        ccp.CCPManager.do_fetch(remote)
-        self.update_service(remote, 'horizon')
-        k8s.K8SManager.create_registry(remote)
-        ccp.CCPManager.do_build(remote, 'builder_push',
-                                registry_address=settings.REGISTRY)
+        k8s_actions.install_k8s()
+        ccp_actions.install_ccp()
+        k8sclient = k8s_actions.get_k8sclient()
+
+        remote = underlay.remote(host=config.k8s.kube_host)
+
+        ccp_actions.do_fetch()
+        ccp_actions.update_service('horizon')
+        k8s_actions.create_registry(remote)
+        ccp_actions.do_build('builder_push',
+                             registry_address=settings.REGISTRY)
         topology_path = os.getcwd() + '/mcp_tests/templates/' \
                                       'k8s_templates/k8s_topology.yaml'
         remote.upload(topology_path, './')
-        ccp.CCPManager.do_deploy(remote,
-                                 registry_address=settings.REGISTRY,
-                                 deploy_config='~/k8s_topology.yaml')
+        ccp_actions.do_deploy(registry_address=settings.REGISTRY,
+                              deploy_config='~/k8s_topology.yaml')
         post_os_deploy_checks.check_jobs_status(k8sclient, timeout=1500,
                                                 namespace='ccp')
         post_os_deploy_checks.check_pods_status(k8sclient, timeout=1500,
@@ -98,3 +96,4 @@ class TestServiceHorizon(base_test.ServiceBase):
             " ./run_tests.sh -V'".format(settings.SSH_LOGIN))
         assert result['exit_code'] == 0,\
             "Horizon unit tests failed, result is {}".format(result)
+        remote.close()
