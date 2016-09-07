30853bc7080ddaf0de71c3148b0cfaa659deca3a
Tatyana Leontovich <tleontovich@mirantis.com>
2016-09-07 20:23:25 +0300

    Add test for nova service
    Also change test_deploy to use latest fixtures changes
    
    Change-Id: I36f41954d122b3f1e1c1ed68778e6c404a32abe0

--- fuel_ccp_tests/tests/system/test_deploy.py
+++ fuel_ccp_tests/tests/system/test_deploy.py
@@ -11,65 +11,26 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import os
 import pytest
 
 import base_test
 from fuel_ccp_tests import logger
 from fuel_ccp_tests import settings
-from fuel_ccp_tests.helpers import post_install_k8s_checks
 from fuel_ccp_tests.helpers import post_os_deploy_checks
 from fuel_ccp_tests.helpers import ext
 
 LOG = logger.logger
 
 
-@pytest.mark.deploy_openstack
-@pytest.mark.revert_snapshot(ext.SNAPSHOT.ccp_deployed)
 class TestDeployOpenstack(base_test.SystemBaseTest):
     """Deploy OpenStack with CCP
 
        pytest.mark: deploy_openstack
     """
-    snapshot_microservices_deployed = 'snapshot_microservices_deployed'
 
-    def get_params(self, params_list, exclude_list=None):
-        params = []
-        for item in params_list:
-            if isinstance(item, dict):
-                if item.keys()[0] not in exclude_list:
-                    params.append(item)
-            else:
-                if item not in exclude_list:
-                    params.append(item)
-        params_dict = {}
-        for item in filter(
-                lambda x: isinstance(x, dict), params):
-            params_dict.update(item)
-        params_list = [item for item in params if not isinstance(
-                       item, dict)]
-        return params_list, params_dict
-
-    def pre_build_deploy_step(self, remote):
-        topology_path = os.path.join(
-            os.getcwd(),
-            'fuel_ccp_tests/templates/k8s_templates/k8s_topology.yaml')
-        remote.upload(topology_path, './')
-        command = '>/var/log/microservices.log'
-        with remote.get_sudo(remote):
-            LOG.info(
-                "Running command '{cmd}' on node {node}".format(
-                    cmd=command,
-                    node=remote.hostname
-                )
-            )
-            result = remote.check_call(command, verbose=True)
-            assert result['exit_code'] == 0
-
-    @pytest.mark.snapshot_needed(name=snapshot_microservices_deployed)
-    @pytest.mark.fail_snapshot
-    def test_fuel_ccp_deploy_microservices(self, config, underlay, ccpcluster,
-                                           k8scluster):
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.ccp_deployed)
+    @pytest.mark.deploy_openstack
+    def test_fuel_ccp_deploy_microservices(self, ccpcluster, k8s_actions):
         """Deploy base environment
 
         Scenario:
@@ -80,27 +41,22 @@ class TestDeployOpenstack(base_test.SystemBaseTest):
 
         Duration 35 min
         """
-        k8sclient = k8scluster.api
-        remote = underlay.remote(host=config.k8s.kube_host)
         if settings.BUILD_IMAGES:
-            k8scluster.create_registry()
+            k8s_actions.create_registry()
             ccpcluster.build()
-            post_install_k8s_checks.check_calico_network(remote, k8sclient)
         else:
             if not settings.REGISTRY:
                 raise ValueError("The REGISTRY variable should be set with "
                                  "external registry address, "
                                  "current value {0}".format(settings.REGISTRY))
         ccpcluster.deploy()
-        post_os_deploy_checks.check_jobs_status(k8sclient, timeout=1500,
-                                                namespace='ccp')
-        post_os_deploy_checks.check_pods_status(k8sclient, timeout=2500,
-                                                namespace='ccp')
+        post_os_deploy_checks.check_jobs_status(k8s_actions.api)
+        post_os_deploy_checks.check_pods_status(k8s_actions.api)
 
-    @pytest.mark.snapshot_needed
-    @pytest.mark.revert_snapshot(ext.SNAPSHOT.k8s_deployed)
-    @pytest.mark.fail_snapshot
-    def test_fuel_ccp_dry_run(self, config, underlay, ccpcluster, k8scluster):
+    @pytest.mark.revert_snapshot(ext.SNAPSHOT.ccp_deployed)
+    @pytest.mark.component
+    @pytest.mark.dry_run
+    def test_fuel_ccp_dry_run(self, ccpcluster, k8s_actions):
         """Deploy base environment
 
         Scenario:
@@ -112,13 +68,10 @@ class TestDeployOpenstack(base_test.SystemBaseTest):
 
         Duration 35 min
         """
-        k8sclient = k8scluster.api
-        k8scluster.create_registry()
+        k8s_actions.create_registry()
         ccpcluster.build()
         export_dir = "/home/{user}/export".format(user=settings.SSH_LOGIN)
         ccpcluster.dry_deploy(export_dir=export_dir)
-        k8scluster.create_objects(folder=export_dir)
-        post_os_deploy_checks.check_jobs_status(k8sclient, timeout=1500,
-                                                namespace='default')
-        post_os_deploy_checks.check_pods_status(k8sclient, timeout=2500,
-                                                namespace='default')
+        k8s_actions.create_objects(folder=export_dir)
+        post_os_deploy_checks.check_jobs_status(k8s_actions.api)
+        post_os_deploy_checks.check_pods_status(k8s_actions.api)
