b3b559cd29862bfa4f3aeca7f0d5ea82d6840b84
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-09-15 14:22:48 +0300

    Add stacklight influxdb test for CPU metrics
    This test check influxdb CPU metrix collection.
    
    Change-Id: I693af970b5c3eaf527102b56424be70fe7506d57

--- fuel_ccp_tests/tests/component/stacklight/test_influxdb.py
+++ fuel_ccp_tests/tests/component/stacklight/test_influxdb.py
@@ -12,6 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import time
 import uuid
 
 import pytest
@@ -45,12 +46,44 @@ def make_file(admin_node):
         admin_node.execute('rm /tmp/{}'.format(filename))
 
 
+@pytest.yield_fixture
+def cpu_workload(admin_node):
+
+    pids = set()
+
+    def _make_workload():
+        pid = admin_node.check_call(
+            'gzip < /dev/zero > /dev/null 2>&1 & echo $!').stdout_str
+        # check process is running
+        cmd = 'ls /proc/{}'.format(pid)
+        err_msg = "Background process with pid `{}` is not found".format(pid)
+        assert admin_node.execute(cmd).exit_code == 0, err_msg
+        pids.add(pid)
+
+    yield _make_workload
+
+    for pid in pids:
+        admin_node.execute('kill {}'.format(pid))
+
+
 @pytest.mark.revert_snapshot(ext.SNAPSHOT.ccp_deployed)
 class TestMetrics(object):
     def get_node_free_space(self, remote, mount_point='/'):
         return int(remote.check_call(
             "df | grep -E {}$ | awk '{{ print $4 }}'".format(
                 mount_point)).stdout_str)
+
+    def _get_cpu_metrics(self, remote):
+        output = remote.check_call('cat /proc/stat | head -n1').stdout_str
+        return map(int, output.split()[1:])
+
+    def get_node_user_cpu_percentage(self, remote):
+        old_values = self._get_cpu_metrics(remote)
+        time.sleep(1)
+        new_values = self._get_cpu_metrics(remote)
+        diff = map(lambda x: x[0] - x[1], zip(new_values, old_values))
+        # user_cpu is 1st value from metrics
+        return diff[0] * 100.0 / sum(diff)
 
     def test_filesystem_metrics(self, make_file, influxdb_actions, admin_node):
         """Check reporting filesystem metrics in InfluxDb
@@ -97,3 +130,77 @@ class TestMetrics(object):
         # Get df free space from admin_node
         free = self.get_node_free_space(admin_node)
         assert free == pytest.approx(data['value'], rel=0.1)
+
+    def test_cpu_metrics(self, influxdb_actions, admin_node, cpu_workload):
+        """Check reporting CPU metrics in InfluxDb
+
+        Scenario:
+            * Get measurements starts with `intel.procfs.cpu`
+            * Get information from influxdb pod for all CPU series:
+                    kubectl exec -it <pod name> -- influx -host <ip> \
+                    -database ccp -execute "select count(value) \
+                    from \"<series>\" where time > now() - 1d"
+            * Check that all series contains some data
+            * Check that cpu usage on influxdb nearly equal to
+                `cat /proc/stat` result
+            * Create small workload on node
+            * Check that cpu usage on influxdb are increased
+        """
+        # Get series
+        series = influxdb_actions.get_measurements('/intel\.procfs\.cpu*/')
+
+        # Check series contains records
+        for serie in series:
+            influxdb_actions.check_serie_contains_records(serie)
+
+        # Get admin_node CPU user_percentage
+        user_percentage_serie = "intel.procfs.cpu.user_percentage"
+        hostname = admin_node.check_call('hostname').stdout_str
+        query_conditions = ("hostname='{hostname}' and cpuID='all'").format(
+            hostname=hostname)
+
+        # Compare last 5 metrics
+        host_percentages = []
+        influxdb_percentages = []
+        kwargs = {}
+        for i in range(5):
+            data = influxdb_actions.get_last_record(
+                user_percentage_serie,
+                conditions=query_conditions,
+                **kwargs)
+            kwargs['updated_after'] = data['time']
+            # Get user_percentage from admin_node
+            user_percentage = self.get_node_user_cpu_percentage(admin_node)
+            host_percentages.append(user_percentage)
+            influxdb_percentages.append(data['value'])
+        err_msg = ("Mean CPU user load for last 5 times from influxdb -  {0} "
+                   "and same value from host - {1} "
+                   "are significantly different").format(
+                       sum(influxdb_percentages) / 5,
+                       sum(host_percentages) / 5)
+        assert sum(host_percentages) == pytest.approx(
+            sum(influxdb_percentages), rel=0.5), err_msg
+
+        # start workload
+        cpu_workload()
+
+        # Compare last 5 metrics
+        host_percentages = []
+        influxdb_percentages = []
+        for i in range(5):
+            data = influxdb_actions.get_last_record(
+                user_percentage_serie,
+                conditions=query_conditions,
+                **kwargs)
+            kwargs['updated_after'] = data['time']
+            # Get user_percentage from admin_node
+            user_percentage = self.get_node_user_cpu_percentage(admin_node)
+            host_percentages.append(user_percentage)
+            influxdb_percentages.append(data['value'])
+        err_msg = ("Mean CPU user load for last 5 times from influxdb -  {0} "
+                   "and same value from host - {1} "
+                   "are significantly different").format(
+                       sum(influxdb_percentages) / 5,
+                       sum(host_percentages) / 5)
+        assert sum(host_percentages) == pytest.approx(
+            sum(influxdb_percentages), rel=0.5), err_msg
