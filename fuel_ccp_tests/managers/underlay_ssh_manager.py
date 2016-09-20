429689a32c9b25e7b978457a76124af8174b727e
Artem Panchenko <apanchenko@mirantis.com>
2016-09-20 14:49:49 +0300

    Add component tests for Calico network plugin
    Scenario:
      1. Install k8s with Calico network plugin.
      2. Run netchecker-server service.
      3. Run netchecker-agent daemon set.
      4. Get network verification status.
         Check status is 'OK'.
    
    New test depends on mcp-netchecker tool. Some
    related to network checker stuff was moved and
    refactored.
    
    Change-Id: I1f1c85a943a937e11f1e61df046d3c1425dd3643

--- fuel_ccp_tests/managers/underlay_ssh_manager.py
+++ fuel_ccp_tests/managers/underlay_ssh_manager.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import random
+
 from devops.helpers import helpers
 from devops.helpers import ssh_client
 from paramiko import rsakey
@@ -309,3 +311,22 @@ class UnderlaySSHManager(object):
                 command=cmd, verbose=verbose, timeout=timeout,
                 error_info=error_info, expected=expected,
                 raise_on_err=raise_on_err)
+
+    def dir_upload(self, host, source, destination):
+        """Upload local directory content to remote host
+
+        :param host: str, remote node name
+        :param source: str, local directory path
+        :param destination: str, local directory path
+        """
+        with self.remote(node_name=host) as remote:
+            remote.upload(source, destination)
+
+    def get_random_node(self):
+        """Get random node name
+
+        :return: str, name of node
+        """
+        nodes = self.node_names()
+        random.shuffle(nodes)
+        return nodes.pop()
