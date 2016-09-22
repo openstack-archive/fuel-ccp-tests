22a59dcc32917255ccd58c012e2434e488d4eac2
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2016-09-22 15:58:23 +0300

    Add reconfig/redeploy test
    Change-Id: Ibd965ac0040e664909f6405290b72065a1183bbd

---
+++ fuel_ccp_tests/tests/component/ccp/test_reconfig.py
@@ -0,0 +1,45 @@
+
+import json
+import pytest
+
+from fuel_ccp_tests import logger
+from fuel_ccp_tests import settings
+from fuel_ccp_tests.helpers import post_os_deploy_checks
+from fuel_ccp_tests.helpers import ext
+
+LOG = logger.logger
+
+
+@pytest.mark.ccp_cli_reconfig
+@pytest.mark.ccp_cli_redeploy
+@pytest.mark.revert_snapshot(ext.SNAPSHOT.ccp_deployed)
+@pytest.mark.component
+def test_deploy_and_reconfig_keystone(config, ccpcluster,
+                                      k8s_actions, underlay):
+
+    ccpcluster.deploy()
+    post_os_deploy_checks.check_jobs_status(k8s_actions.api)
+    post_os_deploy_checks.check_pods_status(k8s_actions.api)
+
+    remote = underlay.remote(host=config.k8s.kube_host)
+    remote.execute('virtualenv ~/venv && '
+                   'source ~/venv/bin/activate && '
+                   'pip install python-openstackclient')
+    res = remote.execute('source ~/venv/bin/activate ; '
+                         'source ~/openrc-ccp; openstack user list -f json')
+    LOG.debug("List of users {}".format(res.stdout_str))
+    users1 = json.loads(res.stdout_str)
+    remote.execute(
+        "echo 'keystone__public_port: 5001' >> {deploy_config}".format(
+            deploy_config=settings.DEPLOY_CONFIG))
+    ccpcluster.deploy('keystone')
+    post_os_deploy_checks.check_jobs_status(k8s_actions.api)
+    post_os_deploy_checks.check_pods_status(k8s_actions.api)
+
+    res = remote.execute('source ~/venv/bin/activate ;'
+                         'source ~/openrc-ccp; openstack user list -f json')
+    LOG.debug("List of users {}".format(res.stdout_str))
+    users2 = json.loads(res.stdout_str)
+
+    remote.close()
+    assert users1 == users2
