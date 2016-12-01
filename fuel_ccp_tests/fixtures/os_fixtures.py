de579f9712f1ecba2f50f3e8a298ce7a098791cc
asledzinskiy <asledzinskiy@mirantis.com>
2016-12-01 15:21:02 +0200

    Add galera tests
    - Add tests on galera deployment, galera node reboot, shutdown,
    poweroff, cold reboot, cluster shutdown
    - Add test on scale up and down galera
    - Galera checkers will be added after merging patch
    https://review.openstack.org/#/c/399999/8/service/files/galera_checker.py
    
    Change-Id: I31e1fe214e558497bcec7661f9e204ee645a56c1

--- fuel_ccp_tests/fixtures/os_fixtures.py
+++ fuel_ccp_tests/fixtures/os_fixtures.py
@@ -12,10 +12,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from copy import deepcopy
+import os
 import pytest
 
 from fuel_ccp_tests import logger
+from fuel_ccp_tests import settings
 from fuel_ccp_tests.helpers import ext
+from fuel_ccp_tests.helpers import post_os_deploy_checks
 from fuel_ccp_tests.managers.osmanager import OSManager
 
 LOG = logger.logger
@@ -50,3 +54,52 @@ def os_deployed(ccpcluster,
     else:
         LOG.info("Openstack allready installed and running...")
         osmanager.check_os_ready()
+
+@pytest.fixture(scope='function')
+def galera_deployed(ccpcluster,
+                    hardware,
+                    underlay,
+                    revert_snapshot,
+                    config,
+                    k8s_actions):
+    """Deploy galera cluster
+    """
+    # If no snapshot was reverted, then try to revert the snapshot
+    # that belongs to the fixture.
+    # Note: keep fixtures in strict dependences from each other!
+    if not revert_snapshot:
+        if hardware.has_snapshot(ext.SNAPSHOT.os_galera_deployed) and \
+                hardware.has_snapshot_config(ext.SNAPSHOT.os_galera_deployed):
+            hardware.revert_snapshot(ext.SNAPSHOT.os_galera_deployed)
+    if not config.os.running:
+        general_config = deepcopy(settings.CCP_CONF)
+        general_config['replicas'] = {'galera': 3}
+        if settings.BUILD_IMAGES:
+            k8s_actions.create_registry()
+            ccpcluster.build()
+        topology_path = \
+            os.getcwd() + '/fuel_ccp_tests/templates/k8s_templates/' \
+                         '3galera_1comp.yaml'
+        remote = underlay.remote(host=config.k8s.kube_host)
+        remote.upload(topology_path, '/tmp')
+        ccpcluster.put_yaml_config('./config_1.yaml', general_config)
+        ccpcluster.add_includes('./config_1.yaml', [
+            settings.CCP_DEPLOY_CONFIG,
+            settings.CCP_SOURCES_CONFIG,
+            '/tmp/3galera_1comp.yaml'])
+
+        underlay.sudo_check_call("pip install python-openstackclient",
+                                 host=config.k8s.kube_host)
+        ccpcluster.deploy(params={"config-file": "./config_1.yaml"},
+                          use_cli_params=True)
+        post_os_deploy_checks.check_jobs_status(k8s_actions.api, timeout=2000)
+        post_os_deploy_checks.check_pods_status(k8s_actions.api)
+        # todo: add invocation of galera checker script
+        remote.check_call(
+            "source openrc-{}; bash fuel-ccp/tools/deploy-test-vms.sh -a"
+            " create".format(
+                settings.CCP_CONF["kubernetes"]["namespace"]),
+            timeout=600)
+
+        config.os.running = True
+        hardware.create_snapshot(ext.SNAPSHOT.os_galera_deployed)
