854fdb2be00bed809057f339a8df04f5bd92036a
Artem Panchenko <apanchenko@mirantis.com>
2016-12-08 16:49:09 +0200

    [DO NOT REVIEW] Check k8s setup with enabled Calico network policies
    Sometimes dnsmasq on node3 doesn't work correctly if Calico network policies
    are enabled in kubernetes. It happens because dnsmasq pods are started before
    Calico CNI binraries are copied to node3 (non-master node), so dnsmasq
    is running in "offline" mode.
    
    This patch is NOT going to be merged and created for temporary test purposes
    only. If you are a reviewer of 'fuel-ccp-tests' - please just ignore it.
    
    Change-Id: I206c13b1443572ac6db58313ad0cbe91125d7c1c

--- fuel_ccp_tests/tests/system/test_calico.py
+++ fuel_ccp_tests/tests/system/test_calico.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import time
+
 import pytest
 
 import base_test
@@ -62,3 +64,52 @@ class TestFuelCCPCalico(base_test.SystemBaseTest):
         # STEP #4
         show_step(4)
         netchecker.wait_check_network(config.k8s.kube_host, works=True)
+
+
+@pytest.mark.usefixtures("check_calico_images_settings")
+class TestFuelCCPCalicoPolicy(base_test.SystemBaseTest):
+    """Test class for Calico network policies in k8s"""
+
+    kube_settings = settings.DEFAULT_CUSTOM_YAML
+    kube_settings['deploy_netchecker'] = False
+    kube_settings['enable_network_policy'] = True
+
+    @pytest.mark.fail_snapshot
+    @pytest.mark.snapshot_needed
+    def test_k8s_netchecker_calico(self, k8scluster, config,
+                                   show_step):
+        """Test for deploying k8s environment with Calico plugin and check
+           network connectivity between pods
+
+        Scenario:
+            1. Install k8s with Calico network plugin.
+            2. Run netchecker-server service.
+            3. Run netchecker-agent daemon set.
+            4. Get network verification status. Check status is 'OK'.
+            5. Repeat step #4 30 times within 30 minutes
+
+        Duration: 3000 seconds
+        """
+
+        # STEP #1
+        show_step(1)
+        k8sclient = k8scluster.api
+        assert k8sclient.nodes.list() is not None, "Can not get nodes list"
+
+        # STEP #2
+        show_step(2)
+        netchecker.start_server(k8s=k8scluster)
+        netchecker.wait_running(config.k8s.kube_host, timeout=240)
+
+        # STEP #3
+        show_step(3)
+        netchecker.start_agent(k8s=k8scluster)
+
+        # STEP #4
+        show_step(4)
+        netchecker.wait_check_network(config.k8s.kube_host, works=True)
+
+        show_step(5)
+        for _ in range(0,30):
+            time.sleep(60)
+            netchecker.wait_check_network(config.k8s.kube_host, works=True)
